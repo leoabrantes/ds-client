15ee43d524f3791960dfe35697b67aa142022e9a
Leo Abrantes <leoabrantesmelo@gmail.com>
2022-05-02 11:36:44 -0300

    Project created and clientClass

---
+++ backend/src/main/java/com/leoabrantes/dsclient/entities/Client.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.leoabrantes.dsclient.entities;
+
+import java.time.Instant;
+
+public class Client {
+	
+	
+	private Long id;
+	private String name;
+	private String cpf;
+	private Double income;
+	private Instant birthDate;
+	private Integer children;
+	
+
+}
