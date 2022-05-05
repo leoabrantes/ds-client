da5c75984148a6c7d8000f44b0854a20f8f36d96
Leo Abrantes <leoabrantesmelo@gmail.com>
2022-05-05 14:16:55 -0300

    Seeding database and DTO

--- backend/src/main/java/com/leoabrantes/dsclient/services/ClientService.java
+++ backend/src/main/java/com/leoabrantes/dsclient/services/ClientService.java
@@ -1,10 +1,13 @@
 package com.leoabrantes.dsclient.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.leoabrantes.dsclient.dto.ClientDTO;
 import com.leoabrantes.dsclient.entities.Client;
 import com.leoabrantes.dsclient.repositories.ClientRepository;
 
@@ -15,7 +18,11 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 	
-	public List<Client> findAll(){
-		return repository.findAll();
+	@Transactional(readOnly = true)
+	public List<ClientDTO> findAll(){
+		List<Client> list = repository.findAll();
+		
+		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
+		
 	}
 }
