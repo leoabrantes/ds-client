ce3e5e6483fd8c62a7c3051edde610f040853056
Leo Abrantes <leoabrantesmelo@gmail.com>
2022-05-07 17:21:52 -0300

    Delete

--- backend/src/main/java/com/leoabrantes/dsclient/services/ClientService.java
+++ backend/src/main/java/com/leoabrantes/dsclient/services/ClientService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.leoabrantes.dsclient.dto.ClientDTO;
 import com.leoabrantes.dsclient.entities.Client;
 import com.leoabrantes.dsclient.repositories.ClientRepository;
+import com.leoabrantes.dsclient.services.exceptions.DatabaseException;
 import com.leoabrantes.dsclient.services.exceptions.ResourceNotFoundException;
 
 
@@ -67,15 +70,15 @@ public class ClientService {
 		}
 	}
 
-//	public void delete(Long id) {
-//		try {
-//		repository.deleteById(id);
-//		}
-//		catch(EmptyResultDataAccessException e) {
-//			throw new ResourceNotFoundException("Id not found" + id);
-//		}
-//		catch(DataIntegrityViolationException e) {
-//			throw new DatabaseException("Integrity violation");
-//		}
-//	}
+	public void delete(Long id) {
+		try {
+		repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found" + id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+	}
 }
